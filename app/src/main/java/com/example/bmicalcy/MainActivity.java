68a14e30dba41e1ca7cb160b95581b38b6f848fd
pushpak <pmeshram1100@gmail.com>
2023-12-28 14:16:28 +0530

    this is my BMI commite

---
+++ app/src/main/java/com/example/bmicalcy/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.bmicalcy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class
+MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editTextWeight = findViewById(R.id.weight);
+        EditText editTextHeight = findViewById(R.id.height);
+        Button button = findViewById(R.id.btnSubmit);
+        TextView textView = findViewById(R.id.result);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float height = Float.parseFloat(String.valueOf(editTextHeight.getText()));
+                float weight = Float.parseFloat(String.valueOf(editTextWeight.getText()));
+                float bmi =weight / (height * height );
+                textView.setText(String.valueOf(bmi));
+
+            }
+        });
+
+
+    }
+}
